26ff69d71cd7143f0c3756a093d5a4d9cb8be8cc
kartik patle <kartikpatle8806@gmail.com>
2025-06-12 12:18:29 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -53,7 +53,8 @@ const session = expressSession({
 app.use(session);
 
 const corsOptions = {
-    origin: 'https://reservation-website-kappa.vercel.app/', // replace with your client's origin
+    origin: ['https://reservation-website-kappa.vercel.app/'],
+    method:['POST','GET'],// replace with your client's origin
     credentials: true,  // This allows the session cookie to be sent back and forth
 };
 
